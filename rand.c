de6a878c9d37c1b9c8f900d92a5318ccc44d02bf
Ivan Lin <ilin00@stuy.edu>
2019-11-12 20:49:43 -0500

    changes

--- rand.c
+++ rand.c
@@ -9,20 +9,61 @@ int randint(){
   int fd = open("/dev/random",O_RDONLY);
   if (fd>0){
     int n = read(fd,&i,sizeof(i));
-    if (n<0) return -1;
-    close("/dev/random")
+    if (n<0){
+		printf("error %d: %s\n", errno, strerror(errno));
+		return -1;
+	}
+    close("/dev/random");
     return i;
   }
-  return -1;
+  else{
+	  printf("error %d: %s\n", errno, strerror(errno));
+	  return 0;
+  }
 }
 
 int main(){
   printf("Generating random numbers:\n");
-  int rand_arr[10];
+  int rand_arr[10], read_arr[10];
   for (int i = 0; i < 10; i++){
     rand_arr[i] = randint();
     printf("random %d: %d\n", i, rand_arr[i]);
   }
-
+  printf("Writing files to file...\n");
+  int fd = open("random_numbers", O_RDWR | O_CREAT, 0666);
+  if (fd < 0){
+	  printf("error %d: %s\n", errno, strerror(errno));
+	  return 0;
+  }
+  fd = write(fd, rand_arr, sizeof(rand_arr));
+  if (fd < 0){
+	  printf("error %d: %s\n", errno, strerror(errno));
+	  return 0;
+  }
+  fd = close("random numbers");
+  if (fd < 0){
+	  printf("error %d: %s\n", errno, strerror(errno));
+	  return 0;
+  }
+  printf("Reading numbers from file...\n");
+  fd = open("random_numbers", O_RDONLY);
+  if (fd < 0){
+	  printf("error %d: %s\n", errno, strerror(errno));
+	  return 0;
+  }
+  fd = read(fd, read_arr, sizeof(rand_arr));
+  if (fd < 0){
+	  printf("error %d: %s\n", errno, strerror(errno));
+	  return 0;
+  }
+  fd = close("random numbers");
+	if (fd < 0){
+	  printf("error %d: %s\n", errno, strerror(errno));
+	  return 0;
+	}
+  printf("Verification that written values were the same:\n");
+  for (int i = 0; i < 10; i++){
+	  printf("random %d: %d\n", i, read_arr[i]);
+  }
   return 0;
 }
